6f22d24edb75237fca3efb63b756ba002d48c18e
iizhuk <iizhuky@gmail.com>
2024-11-21 14:55:51 +0100

    Update initFrameGUI.py ttk

--- gui_frames/initFrameGUI.py
+++ gui_frames/initFrameGUI.py
@@ -1,65 +1,51 @@
 from tkinter import *
 from tkinter import ttk
-from utils import initInstrument,openVisaResource
-		
-class InitFrame(ttk.Frame):
+import matplotlib
+import numpy as np
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk as NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
 
-    def __init__(self, master,inst_handle,app):
-        ttk.Frame.__init__(self, master)
-        self.relief = GROOVE
-        self.grid()
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        self.Widgets()
-        self.inst_handle=inst_handle
-        self.parent=app
 		
-    def Widgets(self):
-        self.gpib_add=StringVar(self)
-        self.reset=IntVar(self)
-        self.inst_name=StringVar(self)
-        self.gpib_add.set(str(17))
+class PltDataFrame(Frame):
+    def __init__(self, master,V_data=[],Cp_data=[],Gp_data=[]):
+        ttk.Frame.__init__(self, master, padding=(10, 10, 10, 10))
+        self.f = Figure(figsize=(8,4), dpi=100)
         
-        main_frame= ttk.Frame(self, borderwidth=2, relief=GROOVE, padding=5)
-        main_frame.grid()
-        main_frame.grid_columnconfigure(0, weight=1)
-        main_frame.grid_rowconfigure(0, weight=1)
-		
-        self.add_name=ttk.Label(main_frame,text='GPIB Address:',padding=1)
-        self.add_name.grid(row=0, column=0, rowspan=1, columnspan=1,
-                       sticky=W + E + N + S)
-  
-        self.add_number=Entry(main_frame, textvariable=self.gpib_add, bg='white')
-        self.add_number.grid(row=0, column=1, rowspan=1, columnspan=1,
-                       sticky=W + E + N + S)
-       
-        self.reset_option = ttk.Checkbutton(main_frame, text="Reset?",variable=self.reset)
-        self.reset_option.grid(row=1, column=1, rowspan=1, columnspan=1,
-                       sticky=W + E + N + S)
-		
-        self.inst_nameL=ttk.Label(main_frame,text='Instrument:')
-        self.inst_nameL.grid(row=2, column=0, rowspan=1, columnspan=1,
-                       sticky=W + E + N + S)
-					   
-        self.inst_nameV=ttk.Label(main_frame,text=self.inst_name.get(),borderwidth=1,relief=SUNKEN,
-                                  font=("Helvetica", 8))
-        self.inst_nameV.grid(row=2, column=1, rowspan=1, columnspan=1,
-                       sticky=W + E + N + S)
-					   
-        self.init_Button=ttk.Button(main_frame,text='Initialize',command=self.init_instrument)
-        self.init_Button.grid(row=3, column=1, rowspan=1, columnspan=1,
-                       sticky=W + E + N + S)
-		
-    def get_handle(self):
-        return self.inst_handle
-		
-    def init_instrument(self):
-        print ("Initializing Instrument.................")
-        self.inst_handle=(openVisaResource(self.gpib_add.get()))
-        self.inst_name.set(initInstrument(self.inst_handle,self.reset.get()))
-        self.inst_nameV.config(text=self.inst_name.get().replace(',','\n'))
-        self.parent.updateInstHandle(self.inst_handle)
-        print ("Done")
-     
-		
-		
+        
+    def updatePlot(self,V_data,Cp_data,Gp_data,xaxis1,xaxis2):
+        self.ax1 = self.f.add_subplot(111)
+        self.ax2 = self.ax1.twinx()
+        
+        self.V_data=V_data
+        self.Cp_data=Cp_data
+        self.Gp_data=Gp_data
+        
+        t = self.V_data 
+        s1 = self.Cp_data
+        s2 = self.Gp_data
+
+        self.ax1.plot(t, s1, 'b-')
+        self.ax1.set_title('Data Plot')
+        self.ax1.set_xlabel('Voltage(V)')
+        self.ax1.set_ylabel(xaxis1, color='b')
+        self.ax1.tick_params('y', colors='b')
+        self.ax1.ticklabel_format(style='sci', scilimits=(0,0))
+        
+        
+        
+        self.ax2.plot(t, s2, 'r.')
+        self.ax2.set_ylabel(xaxis2, color='r')
+        self.ax2.tick_params('y', colors='r')
+        self.ax2.ticklabel_format(style='sci',scilimits=(0,0))
+        
+    
+
+        canvas = FigureCanvasTkAgg(self.f,self)
+        canvas.draw()
+        #canvas.show()
+        canvas.get_tk_widget().pack(expand=True)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self)
+        toolbar.update()
+        canvas._tkcanvas.pack(expand=True)
